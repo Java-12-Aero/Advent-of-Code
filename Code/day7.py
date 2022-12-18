05b1bc29c99c29cc6c3a8a0a2910fcd22fb25e3d
Java-12-Aero <seaposeidon31accts@gmail.com>
2022-12-17 23:24:16 -0800

    day 7

--- Code/day7.py
+++ Code/day7.py
@@ -1,1 +1,25 @@
-file = open("Input\Input7.txt")
+file = open("Input\Input7.txt").read()
+inputs = []
+sizes = {}
+terminal = [line for line in file.split("\n")]
+for line in terminal:
+    if(not line.startswith("$ ls") and not line.startswith("dir")):
+        inputs.append(line)
+stack = []
+for i in range(len(inputs)):
+    line = inputs[i]
+    if("cd .." in line): #closes dir, meaning no more subdirs
+        stack.pop()
+    elif("$ cd" in line and not "cd .." in line):
+        stack.append(i) #adds dir to the tree
+        sizes[i] = 0
+    else: #adds sizes to dir and all parent dirs
+        size = int(line.split()[0])
+        for s in stack:
+            sizes[s] += size
+p1 = sum([sizes[i] for i in sizes if sizes[i] <= 100000])
+unused = 70000000 - sizes[0] #size of root dir
+needed = 30000000 - unused #space needed to be freed
+potentials = [sizes[i] for i in sizes if sizes[i] >= needed]
+p2 = min(potentials)
+print(p2)
